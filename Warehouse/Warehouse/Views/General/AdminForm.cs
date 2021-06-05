6a1c55bae94996a9cda06a002798c615a1cbed3c
JackLas <jackgm555@gmail.com>
2021-06-05 23:17:27 +0300

    add general forms

---
+++ Warehouse/Warehouse/Views/General/AdminForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Warehouse.Views.General
+{
+    public partial class AdminForm : Form
+    {
+        public AdminForm()
+        {
+            InitializeComponent();
+        }
+
+        private void tp_employees_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tb_search_employee_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_add_shelf_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_add_client_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Warehouse/Warehouse/Views/General/GeneralForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Warehouse.Views.General
+{
+    public partial class GeneralForm : Form
+    {
+        public GeneralForm()
+        {
+            InitializeComponent();
+        }
+
+        private void tb_item_search_TextChanged(object sender, EventArgs e)
+        {
+            // update list (maybe create a timer)
+        }
+
+        private void tabs_Selected(object sender, TabControlEventArgs e)
+        {
+            if (e.TabPage.Name == "tp_admin")
+            {
+                this.Hide();
+                Form admin = new AdminForm();
+                admin.ShowDialog();
+                this.Show();
+            }
+        }
+
+        private void btn_new_item_action_Click(object sender, EventArgs e)
+        {
+            // open form to create new action
+        }
+    }
+}

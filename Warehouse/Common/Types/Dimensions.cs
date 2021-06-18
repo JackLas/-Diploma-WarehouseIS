f56288459eb730b4933fd7ad5d16f569929181f5
JackLas <jackgm555@gmail.com>
2021-06-18 22:51:10 +0300

    add CC license

--- Warehouse/Common/Types/Dimensions.cs
+++ Warehouse/Common/Types/Dimensions.cs
@@ -1,4 +1,14 @@
-﻿namespace Common.Types
+﻿/*
+ * Warehouse (c) by Yevhenii Kryvyi
+ *
+ * Warehouse is licensed under a
+ * Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+ *
+ * You should have received a copy of the license along with this
+ * work. If not, see <http://creativecommons.org/licenses/by-nc-nd/4.0/>. 
+ */
+
+namespace Common.Types
 {
     public class Dimensions
     {
--- Warehouse/Warehouse/Controllers/Login/ILoginControllerListener.cs
+++ Warehouse/Warehouse/Controllers/Login/ILoginControllerListener.cs
@@ -1,4 +1,14 @@
-﻿using Common.Types;
+﻿/*
+ * Warehouse (c) by Yevhenii Kryvyi
+ *
+ * Warehouse is licensed under a
+ * Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+ *
+ * You should have received a copy of the license along with this
+ * work. If not, see <http://creativecommons.org/licenses/by-nc-nd/4.0/>. 
+ */
+
+using Common.Types;
 
 namespace Warehouse.Controllers.Login
 {
